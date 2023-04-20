a28f207766249843a025842eebc52cf479c807cf
leozhou <492753662@qq.com>
2023-04-20 14:41:38 +0800

    faet: streaming chat error handle

--- Sources/OpenAISwift/ServerSentEventsHandler.swift
+++ Sources/OpenAISwift/ServerSentEventsHandler.swift
@@ -27,6 +27,11 @@ class ServerSentEventsHandler: NSObject {
 
     func processEvent(_ eventData: Data) {
         do {
+            if let chatErr = try? JSONDecoder().decode(ChatError.self, from: eventData) as ChatError {
+                onEventReceived?(.failure(.chatError(error: chatErr.error)))
+                return
+            }
+            
             let res = try JSONDecoder().decode(OpenAI<StreamMessageResult>.self, from: eventData)
             onEventReceived?(.success(res))
         } catch {
@@ -34,9 +39,9 @@ class ServerSentEventsHandler: NSObject {
         }
     }
     
-    // Identify half json
-    func isPartialJson(_ jsonString: String) -> Bool {
-        if jsonString.isEmpty {
+    // Identify full json
+    func isFullJson(_ jsonString: String) -> Bool {
+        if jsonString.isEmpty || (!jsonString.hasPrefix("{") && !jsonString.hasSuffix("}")) {
             return false
         }
 
@@ -50,7 +55,7 @@ class ServerSentEventsHandler: NSObject {
             }
         }
         
-        return bracesCount != 0
+        return bracesCount == 0
     }
 }
 
@@ -60,6 +65,15 @@ extension ServerSentEventsHandler: URLSessionDataDelegate {
     /// The JSON look liks this:
     /// `data: {"id":"chatcmpl-6yVTvD6UAXsE9uG2SmW4Tc2iuFnnT","object":"chat.completion.chunk","created":1679878715,"model":"gpt-3.5-turbo-0301","choices":[{"delta":{"role":"assistant"},"index":0,"finish_reason":null}]}`
     /// `data: {"id":"chatcmpl-6yVTvD6UAXsE9uG2SmW4Tc2iuFnnT","object":"chat.completion.chunk","created":1679878715,"model":"gpt-3.5-turbo-0301","choices":[{"delta":{"content":"Once"},"index":0,"finish_reason":null}]}`
+    /// The erroe JSON look like this:
+    /// {
+    ///     "error": {
+    ///         "message": "Rate limit reached for default-gpt-3.5-turbo in organization org-lPcJIJx90ZmUZfVQ59mquulx on requests per min. Limit: 3 / min. Please try again in 20s. Contact support@openai.com if you continue to have issues. Please add a payment method to your account to increase your rate limit. Visit https://platform.openai.com/account/billing to add a payment method.",
+    ///         "type": "requests",
+    ///         "param": null,
+    ///         "code": null
+    ///     }
+    /// }
     func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
         if let eventString = String(data: data, encoding: .utf8) {
             let lines = eventString.split(separator: "\n")
@@ -70,26 +84,38 @@ extension ServerSentEventsHandler: URLSessionDataDelegate {
                 }
                 if line.hasPrefix("data:"){
                     let jsonData = String(line.dropFirst(5))
-                    if isPartialJson(jsonData) {
-                        cacheBuffer.append(jsonData)
-                    } else if let eventData = jsonData.data(using: .utf8) {
-                        processEvent(eventData)
+                    if isFullJson(jsonData) {
+                        if let eventData = jsonData.data(using: .utf8) {
+                            processEvent(eventData)
+                        }
                         cacheBuffer.removeAll()
+                    } else {
+                        cacheBuffer.append(jsonData)
                     }
                 } else {
                     let unknownData = String(line)
-                    if isPartialJson(unknownData) {
+                    if unknownData.isEmpty {
+                        continue
+                    }
+                    if isFullJson(unknownData) {
+                        if let eventData = unknownData.data(using: .utf8) {
+                            processEvent(eventData)
+                        }
+                        cacheBuffer.removeAll()
+                    } else {
+                        // partial json data
                         var jsonData = ""
                         for bufferData in cacheBuffer {
                             jsonData.append(bufferData)
                         }
                         jsonData.append(unknownData)
-                        
-                        if isPartialJson(jsonData){
-                            cacheBuffer.append(unknownData)
-                        } else if let eventData = jsonData.data(using: .utf8) {
-                            processEvent(eventData)
+                        if isFullJson(jsonData) {
+                            if let eventData = jsonData.data(using: .utf8) {
+                                processEvent(eventData)
+                            }
                             cacheBuffer.removeAll()
+                        } else {
+                            cacheBuffer.append(unknownData)
                         }
                     }
                 }
